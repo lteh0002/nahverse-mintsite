27be4be236a109a0973c9eed6238dfa95c206445
lteh0002 <tehlinhui@gmail.com>
2023-05-23 23:43:55 +0800

    Fix loading page load too fast

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,12 +7,15 @@
   let isLoading = true;
 
   onMount(() => {
-    if (document.readyState === 'complete') {
+    setTimeout(loading, 3000);
+    function loading() {
+      if (document.readyState === 'complete') {
       isLoading = false; // Set isLoading to false if the page has already finished loading
     } else {
       window.addEventListener('load', () => {
         isLoading = false; // Set isLoading to false when the page has finished loading
       });
+    }
     }
   });
 </script>
